eba6d979a958b39f321f25d11bd069792391b3e5
Rustic-Citrus <mrcurtis.english@gmail.com>
2023-07-05 13:25:28 -0300

    Made code more concise
    Added type hints and docstrings

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,44 +1,30 @@
+from typing import Callable
 import numpy as np
 
-def calculate(list):
-    if len(list) == 9:
-        array = np.array(
-            [
-                [num for num in list[:3]],
-                [num for num in list[3:6]],
-                [num for num in list[6:len(list)]]
-            ]
-        )
+def calculate(values: list) -> dict:
+    """Takes a list of 9 integers and splits them into a list of lists, each 
+    containing three integers. Then, it converts the list of lists into a 3x3 
+    Numpy array. Finally, the function calculates the max, min and sum along 
+    both the x and y-axis, as well as a flattened matrix, and returns a 
+    dictionary with the aggregate statistics."""
+    def calculation_matrix(func: Callable[[np.ndarray], np.ndarray]) -> list:
+        """Takes the 3x3 Numpy array, carries out an aggregate function across 
+        the x and y-axes and as a flattened matrix. Finally, it returns a list 
+        of three numbers."""
+        return [[func(array[:3, 0]), func(array[:3, 1]), func(array[:3, 2])],
+                [func(array[0, :3]), func(array[1, :3]), func(array[2, :3])],
+                func(array)]
     
+    if len(values) == 9:
+        array = np.array([[num for num in values[:3]], 
+                          [num for num in values[3:6]], 
+                          [num for num in values[6:len(values)]]])
     else: 
         raise ValueError("List must contain nine numbers.")
-        
-    def op_matrix(op):
-        return [
-            [
-                op(array[:3, 0]),
-                op(array[:3, 1]),
-                op(array[:3, 2])            
-            ],
-            [
-                op(array[0, :3]),
-                op(array[1, :3]),
-                op(array[2, :3])
-            ],
-            op(array)
-        ]
-    
     calculations = {}
-    op_dict = {
-        "mean": np.mean, 
-        "variance": np.var, 
-        "standard deviation": np.std, 
-        "max": np.amax, 
-        "min": np.amin, 
-        "sum": np.sum
-    }
-    
-    for op_name, op_func in op_dict.items():
-        calculations[op_name] = op_matrix(op_func)
-
+    agg_statistics = {"mean": np.mean, "variance": np.var, 
+                      "standard deviation": np.std, "max": np.amax, 
+                      "min": np.amin, "sum": np.sum}
+    for stat_name, agg_func in agg_statistics.items():
+        calculations[stat_name] = calculation_matrix(agg_func)
     return calculations
